1fb8683a8b34aa216f73b1fb41aec4353679bd2b
Leni Kadali Mutungi <lenikmutungi@gmail.com>
2018-07-17 19:00:52 +0300

    ch: Remove unnecessary test code for RequestClass
    Removed unnecessary test code for the RequestClass since
    the functionality it is testing has been moved to the req_helper
    module and its corresponding tests in the test_req_helper module.

--- project/tests/test_req_class.py
+++ project/tests/test_req_class.py
@@ -16,22 +16,3 @@ class TestRequest(unittest.TestCase):
         self.assertTrue(hasattr(self.request_class, 'req_name'), msg=AttributeError)
         self.assertTrue(hasattr(self.request_class, 'req_type'), msg=AttributeError)
         self.assertTrue(hasattr(self.request_class, 'req_desc'), msg=AttributeError)
-
-    def test_req_data_type(self):
-        self.request_class = RequestClass(1, 'somename', 'sometype', 'somedescription')
-        self.assertIsInstance(self.request_class.req_id, int, msg=ValueError)
-        self.assertIsInstance(self.request_class.req_name, str, msg='Request name is not a string')
-        self.assertIsInstance(self.request_class.req_type,  str, msg='Request type is not a string')
-        self.assertIsInstance(self.request_class.req_desc,  str, msg='Request description is not a string')
-
-    def test_req_not_null_field(self):
-        self.request_class = RequestClass(0, '', '', '')
-        self.assertEqual(self.request_class.req_id, 0, msg='ID cannot be zero')
-        self.assertEqual(len(self.request_class.req_name), 0, msg='Name is empty')
-        self.assertEqual(len(self.request_class.req_type), 0, msg='Type is empty')
-        self.assertEqual(len(self.request_class.req_desc), 0, msg='Description is empty')
-
-    def test_invalid_input(self):
-        with self.assertRaises(ValueError):
-            self.request_class = RequestClass('', 1, 1, 1)
-            self.request_class = RequestClass(5, 'something', 1, 1)
