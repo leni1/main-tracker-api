71c8002f62ac94f5cdd123f7f8b83c11f6bc9932
Leni Kadali Mutungi <lenikmutungi@gmail.com>
2018-07-17 18:59:10 +0300

    ch: Remove validation code from RequestClass
    Removed validation code from RequestClass. The logic is now in
    the req_helper module.

--- project/api/models/request_class.py
+++ project/api/models/request_class.py
@@ -8,20 +8,7 @@ class RequestClass(object):
     req_desc: string value that is the request description"""
 
     def __init__(self, req_id, req_name, req_type, req_desc):
-        if not isinstance(req_id, int):
-            raise ValueError
-        elif not isinstance(req_name, str):
-            raise ValueError
-        elif not isinstance(req_name, str):
-            raise ValueError
-        elif not isinstance(req_desc, str):
-            raise ValueError
-        else:
-            if (req_id != 0 and len(req_name !=0)
-                and len(req_type) != 0 and len(req_desc) != 0):
-                self.req_id = int(req_id)
-                self.req_name = str(req_name)
-                self.req_type = str(req_type)
-                self.req_desc = str(req_desc)
-            else:
-                raise "ID, name, type or description is an empty or zero value."
+            self.req_id = int(req_id)
+            self.req_name = str(req_name)
+            self.req_type = str(req_type)
+            self.req_desc = str(req_desc)
