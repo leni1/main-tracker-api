33826c4b9350fe53e91dc09f6bd324c94f574e4e
Leni Kadali Mutungi <lenikmutungi@gmail.com>
2018-08-04 18:44:38 +0300

    ft: Add get request by id and get all requests endpoints
    Added the get request by id and get all requests endpoints

--- api/views/request_view.py
+++ api/views/request_view.py
@@ -7,6 +7,7 @@ request_handler = RequestHelper()
 
 req = Blueprint('req_view', __name__)
 
+
 @req.route('/users/requests', methods=['POST'])
 def create_request():
     json_data = request.get_json()
@@ -15,7 +16,18 @@ def create_request():
     request_desc = json_data['description']
     request_id = random.randint(1, 10000)
 
-    new_req = request_handler.create_request(request_id, request_name, request_type, request_desc)
+    new_req = request_handler.create_request(
+              request_id, request_name, request_type, request_desc)
     if new_req:
         return jsonify(new_request=vars(new_req)), 201
     return jsonify(message='No request created'), 400
+
+@req.route('/users/requests', methods=['GET'])
+def fetch_all_requests():
+    all_requests = request_handler.fetch_all_requests()
+    return jsonify({'All requests': all_requests}), 200
+
+@req.route('/users/requests/<int:requestId>', methods=['GET'])
+def fetch_request_id(requestId):
+    result = request_handler.fetch_by_id_request(requestId)
+    return jsonify({'Request found': vars(result)}), 200
