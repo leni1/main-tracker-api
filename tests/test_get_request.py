be0e62f6ff3988e141b025966254d95846764f7b
Leni Kadali Mutungi <lenikmutungi@gmail.com>
2018-08-04 18:45:50 +0300

    ft: Add tests for the get request endpoints
    Added tests for the get request endpoints.

---
+++ tests/test_get_request.py
@@ -0,0 +1,45 @@
+import json
+import unittest
+from api import create_app
+from api.models.req_helper import RequestHelper
+
+
+class MaintenanceViews(unittest.TestCase):
+    """Tests the enpoints contains in request_views.py"""
+
+    def setUp(self):
+        self.test_app = create_app({'TESTING': True})
+        self.test_request = self.test_app.test_client()
+        with self.test_app.app_context():
+            self.req_db = RequestHelper.req_db
+
+    def tearDown(self):
+        self.req_db
+
+    def test_get_business(self):
+        self.test_request.post(
+            '/users/requests',
+            content_type='application/json',
+            data=json.dumps(
+                dict(
+                    name='Failing Test',
+                    req_type='Test',
+                    description='A failing test')))
+        response = self.test_request.get('/users/requests',
+                                         content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_business(self):
+        new_req = self.test_request.post(
+            '/users/requests',
+            content_type='application/json',
+            data=json.dumps(
+                dict(
+                    name='Failing Test',
+                    req_type='Test',
+                    description='A failing test')))
+        req_id = json.loads(new_req.get_data())['new_request']['req_id']
+        response = self.test_request.get(
+            '/users/requests/' + str(req_id),
+            content_type='application/json')
+        self.assertEqual(response.status_code, 200)
