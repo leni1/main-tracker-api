9b20c303b10df182f45966748516db2051865dec
Leni Kadali Mutungi <lenikmutungi@gmail.com>
2018-08-07 15:45:47 +0300

    ch: Remove vars from response, rename RequestHelper instance
    Removed vars from the responses given by the API. Renamed
    the RequestHelper instance to request_helper for readability.

--- api/views/request_view.py
+++ api/views/request_view.py
@@ -3,7 +3,7 @@ from flask import (
     Blueprint, jsonify, request)
 
 from api.models.req_helper import RequestHelper
-request_handler = RequestHelper()
+request_helper = RequestHelper()
 
 req = Blueprint('req_view', __name__)
 
@@ -16,18 +16,18 @@ def create_request():
     request_desc = json_data['description']
     request_id = random.randint(1, 10000)
 
-    new_req = request_handler.create_request(
+    new_req = request_helper.create_request(
               request_id, request_name, request_type, request_desc)
     if new_req:
-        return jsonify(new_request=vars(new_req)), 201
+        return jsonify(new_request=new_req), 201
     return jsonify(message='No request created'), 400
 
 @req.route('/users/requests', methods=['GET'])
 def fetch_all_requests():
-    all_requests = request_handler.fetch_all_requests()
+    all_requests = request_helper.fetch_all_requests()
     return jsonify({'All requests': all_requests}), 200
 
 @req.route('/users/requests/<int:requestId>', methods=['GET'])
 def fetch_request_id(requestId):
-    result = request_handler.fetch_by_id_request(requestId)
-    return jsonify({'Request found': vars(result)}), 200
+    result = request_helper.fetch_by_id_request(requestId)
+    return jsonify({'Request found': result}), 200
