adfa29f3284d2085731d454c57b4b5bb4e8b32b5
Leni Kadali Mutungi <lenikmutungi@gmail.com>
2018-08-26 11:24:06 +0300

    ch: Add docstrings as per NumPy/SciPy to req_helper
    Added doc strings to the class and its methods using NumPy/SciPy
    docstring formats as a guide to the req_helper module

--- api/models/req_helper.py
+++ api/models/req_helper.py
@@ -1,4 +1,4 @@
-"""Request Helper class"""
+"""Request Helper module"""
 import re
 
 from .request_class import RequestClass
@@ -6,13 +6,48 @@ from .request_class import RequestClass
 
 class RequestHelper(object):
     """
-    req_db: a list storing requests
+    Helper class that handles creating and modifying
+    RequestClass objects that represent requests in the system.
+
+    Attributes:
+        req_db: List storing requests
+        check_request: Method that checks request details
+        create_request: Method that creates a request
+        change_request: Method that changes a request's details.
+        fetch_id_by_request: Method that returns a request matching
+        a given id.
+        fetch_all_requests: Method that returns all requests present.
     """
+
     req_db = []
 
     def check_request(self, req_id, req_name, req_type, req_desc):
-        """Method for checking that the request's id, name, type
-        and description have valid data types."""
+        """Checks that the request's id, name, type and description
+        have valid data types.
+
+        Makes sure that the request id is an integer, and its name,
+        type, and description are strings matching the patterns
+        supplied.
+
+        Args:
+            req_id: Request id number
+            req_name: Request name.
+            req_type: Request type.
+            req_desc: Request description.
+
+        Returns:
+            False: if the request id is 0 and the request name,
+            type, and description do not match the string patterns
+            supplied.
+            True: if the request id is not 0 and the request name,
+            type, and description match the string patterns supplied.
+
+        Raises:
+            ValueError: The request id was not an integer.
+            TypeError: The request name, type or description was not
+            a string.
+        """
+
         str_args = [req_name, req_type, req_desc]
         if not isinstance(req_id, int):
             raise ValueError
@@ -33,18 +68,45 @@ class RequestHelper(object):
         return False
 
     def create_request(self, req_id, req_name, req_type, req_desc):
-        """Method for creating a request.
+        """Creates a request.
+
         Takes a request's id, name, type and description
-        and creates a RequestClass object that is appended to req_db."""
+        and creates a RequestClass object that is appended to req_db.
+
+        Args:
+            req_id: Request id number
+            req_name: Request name.
+            req_type: Request type.
+            req_desc: Request description.
+
+        Returns:
+            The created RequestClass object in dictionary form i.e.
+            vars(some_obj)
+        """
+
         if self.check_request(req_id, req_name, req_type, req_desc):
             new_req = RequestClass(req_id, req_name, req_type, req_desc)
             self.req_db.append(new_req)
             return vars(new_req)
 
-    def change_request(self, req_id, new_name=None, new_type=None, new_desc=None):
-        """Method for modifyng a request.
+    def change_request(self, req_id,
+                       new_name=None, new_type=None, new_desc=None):
+        """Changes a request.
+
         Takes a request's id, the new name, type and description
-        and updates the RequestClass object with the request id specified."""
+        and updates the RequestClass object with the request id specified.
+
+        Args:
+            req_id: Request id number
+            req_name: Request name.
+            req_type: Request type.
+            req_desc: Request description.
+
+        Returns:
+            The updated RequestClass object in dictionary form i.e.
+            vars(some_obj)
+        """
+
         if self.check_request(req_id, new_name, new_type, new_desc):
             for req in self.req_db:
                 if req_id == req.req_id:
@@ -64,16 +126,42 @@ class RequestHelper(object):
         return False
 
     def fetch_by_id_request(self, req_id):
-        """Method for getting a request by id.
+        """Returns a request with matching id.
+
         Takes a request's id and returns a matching
-        RequestClass object."""
+        RequestClass object.
+
+        Args:
+            req_id: Request id number
+
+        Returns:
+            The matching RequestClass object in dictionary form i.e.
+            vars(some_obj)
+            If no matching object is found, it returns the message
+            'Request not found'.
+
+        Raises:
+            ValueError: The request id is not an integer.
+        """
+
         if not isinstance(req_id, int):
-            raise TypeError
+            raise ValueError
         for req in self.req_db:
             if req_id == req.req_id:
                 return vars(req)
         return 'Request not found'
 
     def fetch_all_requests(self):
-        """Method for returning all requests in req_db."""
+        """Returns all requests.
+
+        Takes a request's id and returns a matching
+        RequestClass object.
+
+        Args:
+            self: RequestHelper class instance
+
+        Returns:
+            All RequestClass objects found in req_db in a list of
+            dictionaries via vars(some_obj).
+        """
         return [vars(req) for req in self.req_db]
