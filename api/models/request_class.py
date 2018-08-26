13592205f1764b6d9dd87a8fb451b06fee3506cc
Leni Kadali Mutungi <lenikmutungi@gmail.com>
2018-08-26 11:30:03 +0300

    ch: Add docstrings to request_class as per NumPy/SciPy
    Added docstrings to the request_class module as per
    NumPy/SciPy standards.

--- api/models/request_class.py
+++ api/models/request_class.py
@@ -1,12 +1,17 @@
-"""Class for the Request"""
+"""Request class module"""
 
 
 class RequestClass(object):
     """
-    req_id: integer that identifies a request
-    req_name: string value that is the request name
-    req_type: string value that is the request type
-    req_desc: string value that is the request description"""
+    Request class object
+
+    The class that represents requests in the system.
+
+    Attributes:
+        req_id: integer that identifies a request
+        req_name: string value that is the request name
+        req_type: string value that is the request type
+        req_desc: string value that is the request description"""
 
     def __init__(self, req_id, req_name, req_type, req_desc):
             self.req_id = int(req_id)
